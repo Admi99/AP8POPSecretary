471a4d1d4b5765b73e389b3312d8a9fa43578b6c
Adam Michálek <a_michalek@utb.cz>
2021-03-15 19:47:49 +0100

    Start Groups Managment view

--- AP8POSecretary/ViewModels/GroupsManagmentViewModel.cs
+++ AP8POSecretary/ViewModels/GroupsManagmentViewModel.cs
@@ -2,6 +2,7 @@
 using AP8POSecretary.Domain.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace AP8POSecretary.ViewModels
@@ -11,10 +12,55 @@ namespace AP8POSecretary.ViewModels
         private readonly IDataService<Group> _groupDataService;
         private readonly IDataService<Subject> _subjectDataService;
 
+        private ObservableCollection<Group> _groups;
+        public ObservableCollection<Group> Groups
+        {
+            get
+            {
+                return _groups;
+            }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged(nameof(Groups));
+            }
+        }
+
         public GroupsManagmentViewModel(IDataService<Group> groupDataService, IDataService<Subject> subjectDataService)
         {
             _groupDataService = groupDataService;
             _subjectDataService = subjectDataService;
+
+            Groups = new ObservableCollection<Group>()
+            {
+                new Group()
+                {
+                    Shortcut="ahoj",
+                    Language="Zdar",
+                    SemesterType = SemesterType.WINTER,
+                    StudentsCount = 10,
+                    StudyYear = 20,
+                    StudyType = StudyType.DAILY
+                },
+                new Group()
+                {
+                    Shortcut="ahoj",
+                    Language="Zdar",
+                    SemesterType = SemesterType.WINTER,
+                    StudentsCount = 10,
+                    StudyYear = 20,
+                    StudyType = StudyType.DAILY
+                },
+                   new Group()
+                {
+                    Shortcut="ahoj",
+                    Language="Zdar",
+                    SemesterType = SemesterType.WINTER,
+                    StudentsCount = 10,
+                    StudyYear = 20,
+                    StudyType = StudyType.DAILY
+                }
+            };
         }
     }
 }
